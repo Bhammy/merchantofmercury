3214b2b02a4e0bb1a4ff86cf1c9187ee2209f506
Brendan Hamill <brenhamill@gmail.com>
2018-01-17 13:51:17 -0500

    adds webpack and some base files

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  context: __dirname, //refers to the root of the project folder
+  entry: './game.js', //imports fro,
+  output: {
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '*'],
+  },
+  devtool: 'source-map',
+};
+
+module.exports = config;
